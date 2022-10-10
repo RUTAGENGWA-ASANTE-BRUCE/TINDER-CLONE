8cae624df3448ea0a6a0b932affa7891532bec3e
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-10-10 15:45:49 +0200

    fix: adding Rapid API NFT images

--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -6,26 +6,28 @@ import axios from "./axios";
 function TinderCards() {
   const [data, setData] = useState([]);
 
-  useEffect(() => {const options = {
-    method: 'GET',
-    url: 'https://opensea13.p.rapidapi.com/assets',
-    params: {
-      collection_slug: 'cryptopunks',
-      order_direction: 'desc',
-      limit: '20',
-      include_orders: 'false'
-    },
-    headers: {
-      'X-RapidAPI-Key': '4580762247mshb4a711a634972ffp17767bjsnadc23ee59733',
-      'X-RapidAPI-Host': 'opensea13.p.rapidapi.com'
-    }
-  };
-  
-  axios.request(options).then(function (response) {
-      console.log(response.data);
-  }).catch(function (error) {
-      console.error(error);
-  });
+  useEffect(() => {
+    async function fetchData() {
+        const options = {
+            method: 'GET',
+            url: 'https://opensea13.p.rapidapi.com/assets',
+            params: {
+              collection_slug: 'cryptopunks',
+              order_direction: 'desc',
+              limit: '20',
+              include_orders: 'false'
+            },
+            headers: {
+              'X-RapidAPI-Key': '4580762247mshb4a711a634972ffp17767bjsnadc23ee59733',
+              'X-RapidAPI-Host': 'opensea13.p.rapidapi.com'
+            }
+          };
+          
+          axios.request(options).then(function (response) {
+              console.log(response.data);
+          }).catch(function (error) {
+              console.error(error);
+          });
     }
     fetchData();
   }, []);
@@ -43,16 +45,16 @@ function TinderCards() {
           data.map((single) => (
             <TinderCard
               className="swipe"
-              key={single.thumbnail}
+              key={single.id}
               preventSwipe={["up", "down"]}
-              onSwipe={(dir) => swiped(dir, single.thumbnail)}
-              onCardLeftScreen={() => outOfFrame(single.thumbnail)}
+              onSwipe={(dir) => swiped(dir, single.id)}
+              onCardLeftScreen={() => outOfFrame(single.id)}
             >
               <div
-                style={{ backgroundImage: `url( ${single.thumbnail})` }}
+                style={{ backgroundImage: `url( ${single.image_url})` }}
                 className="card"
               >
-                <h3 style={{ color: `red` }}>{single.duration}</h3>
+                <h3 style={{ color: `red` }}>{single.name}</h3>
               </div>
             </TinderCard>
           ))}
