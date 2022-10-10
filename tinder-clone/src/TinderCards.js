51ef3e6755aaf882dab2ce38e1c5b89656af8aca
RUTAGENGWA-ASANTE-BRUCE <96545712+RUTAGENGWA-ASANTE-BRUCE@users.noreply.github.com>
2022-10-10 15:11:38 +0200

    fix: adding rapid API

--- tinder-clone/src/TinderCards.js
+++ tinder-clone/src/TinderCards.js
@@ -5,18 +5,34 @@ import TinderCard from "react-tinder-card"
 import axios from './axios'
 function TinderCards ()
 {
-    const [people, setPeople] = useState([])
+    const [data, setData] = useState([])
 
     useEffect(() =>
     {
         async function fetchData ()
         {
-            const req = await axios.get("/tinder/cards")
-            setPeople(req.data)
+            const axios = require("axios");
+
+const options = {
+  method: 'GET',
+  url: 'https://pinterest-video-and-image-downloader.p.rapidapi.com/pinterest',
+  params: {url: 'https://in.pinterest.com/pin/1095852521805152932/'},
+  headers: {
+    'X-RapidAPI-Key': '4580762247mshb4a711a634972ffp17767bjsnadc23ee59733',
+    'X-RapidAPI-Host': 'pinterest-video-and-image-downloader.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+    setData(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
         }   
         fetchData()
     }, [])
-    console.log(people)
+    console.log(data)
     const swiped = (direction, nameToDelete) =>
     {
         console.log("Removing " + nameToDelete)
@@ -28,17 +44,17 @@ function TinderCards ()
     return (
         <div className='TinderCards'>
             <div className='tinderCards__Cardcontainer'>
-                {people.map((person) => (
+                {data.length>0 && data.map((single) => (
                     <TinderCard
                         className='swipe'
-                        key={person.name}
+                        key={single.thumbnail}
                         preventSwipe={["up", "down"]}
-                        onSwipe={(dir) => swiped(dir, person.name)}
-                        onCardLeftScreen={() => outOfFrame(person.name)}
+                        onSwipe={(dir) => swiped(dir, single.thumbnail)}
+                        onCardLeftScreen={() => outOfFrame(single.thumbnail)}
                     >
-                        <div style={{ backgroundImage: `url( ${person.imgUrl})` }}
+                        <div style={{ backgroundImage: `url( ${single.thumbnail})` }}
                             className='card'>
-                            <h3 style={{ color: `red` }}>{person.name}</h3>
+                            <h3 style={{ color: `red` }}>{single.duration}</h3>
 
                         </div>
                     </TinderCard>
